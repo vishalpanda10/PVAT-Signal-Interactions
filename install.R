c9a1ac781d6d75b6ca200e154da614ddea4812ba
Vishal Panda <107870626+vishalpanda10@users.noreply.github.com>
2024-05-18 14:44:02 -0400

    Add install.R for R package installation

---
+++ install.R
@@ -0,0 +1,6 @@
+# List of required packages
+packages <- c("nichenetr", "Seurat", "SeuratObject", "tidyverse", "dplyr", "circlize", "networkD3")
+
+
+# Load packages
+lapply(packages, library, character.only = TRUE)
